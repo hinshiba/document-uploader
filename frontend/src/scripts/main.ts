d4a252c2d5a1cd04e6e156d1826f13a73bf7e075
kobita <torien2846@gmail.com>
2026-06-27 21:01:23 +0900

    htmlを追加

--- frontend/src/scripts/main.ts
+++ frontend/src/scripts/main.ts
@@ -55,14 +55,16 @@ fetchFaculties()
 const file = document.getElementById("file") as HTMLInputElement;
 const uploadbtn = document.getElementById("uploadbtn")
 
-// console.log(file.files);
+// 送信ボタンを押した後にユーザが入力したデータをformDataに内容を保存して得られたすべての情報を送信する
 uploadbtn?.addEventListener("click", async () => {
-    const filedata = file.files?.[0];
-
-    if (!filedata) return;
+    const alldata = file.files;
+    if (!alldata || alldata.length === 0) return;
 
     const formData = new FormData();
-    formData.append("file", filedata);
+
+    for (const filedata of alldata) {
+        formData.append("file", filedata)
+    }
     // formData.append("metaData",JSON.stringify(metadata))
 
     await fetch("../../../docs", {
