e46897c711e5e88b4e65f6388f35873e945ecc56
kakkak444 <gpg@mail.glabyl.com>
2026-06-29 03:54:31 +0900

    fix: `Subject`をAPI仕様に従うように修正

--- backend/src/domain/subject.rs
+++ backend/src/domain/subject.rs
@@ -1,21 +1,37 @@
 use super::{
     Id,
+    Grade,
+    Term,
     faculty::Faculty,
+    major::Major,
 };
 
 #[derive(Debug, Hash)]
 pub struct Subject {
     id: Id<Subject>,
     name: String,
-    faculty_id: Option<Id<Faculty>>,
+    faculty_id: Id<Faculty>,
+    major_id: Id<Major>,
+    grade: Grade<Subject>,
+    term: Term<Subject>,
 }
 
 impl Subject {
-    pub fn new(id: Id<Subject>, name: String, faculty_id: Option<Id<Faculty>>) -> Self {
+    pub fn new(
+        id: Id<Subject>,
+        name: String,
+        faculty_id: Id<Faculty>,
+        major_id: Id<Major>,
+        grade: Grade<Subject>,
+        term: Term<Subject>,
+    ) -> Self {
         Self {
             id,
             name,
             faculty_id,
+            major_id,
+            grade,
+            term,
         }
     }
     pub fn id(&self) -> &Id<Subject> {
@@ -24,7 +40,16 @@ impl Subject {
     pub fn name(&self) -> &str {
         &self.name
     }
-    pub fn faculty_id(&self) -> Option<&Id<Faculty>> {
-        self.faculty_id.as_ref()
+    pub fn faculty_id(&self) -> &Id<Faculty> {
+        &self.faculty_id
+    }
+    pub fn major_id(&self) -> &Id<Major> {
+        &self.major_id
+    }
+    pub fn grade(&self) -> &Grade<Subject> {
+        &self.grade
+    }
+    pub fn term(&self) -> &Term<Subject> {
+        &self.term
     }
 }
