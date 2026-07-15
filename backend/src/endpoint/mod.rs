a7eb0a7d1a276545996e99ee327a4f07fdf2a613
kakkak444 <gpg@mail.glabyl.com>
2026-07-15 22:20:17 +0900

    add: エラー型を追加

--- backend/src/endpoint/mod.rs
+++ backend/src/endpoint/mod.rs
@@ -1,1 +1,11 @@
+use serde::Serialize;
+
 pub mod alive;
+
+#[derive(Debug, Clone, Hash, Serialize)]
+pub struct EndpointError {
+    pub message: String,
+    pub details: Option<String>,
+}
+
+pub type EndpointResult<T> = Result<T, EndpointError>;
