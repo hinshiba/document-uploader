b162536b0ab561cd409c9383ea25704b241afa4d
kakkak444 <gpg@mail.glabyl.com>
2026-07-16 22:19:51 +0900

    add: サーバを実装

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,14 +3,24 @@ mod endpoint;
 mod infrastructure;
 mod usecase;
 
+use std::sync::Arc;
+use infrastructure::repository::ExampleRepository;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     init_tracing_subscriber();
 
+    let repo = Arc::new(ExampleRepository::new("./test".into())?);
+
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
 
     let app = axum::Router::new()
-        .route("/", axum::routing::get(|| async { "Hello, World!" }));
+        .route("/", axum::routing::get(|| async { "Hello, World!" }))
+        .route("/alive", axum::routing::get(endpoint::alive::alive))
+        .route("/faculties", axum::routing::get(endpoint::faculties::get_faculties::<Arc<_>>))
+        .route("/subjects", axum::routing::get(endpoint::subjects::get_subjects::<Arc<_>>))
+        .route("/docs", axum::routing::post(endpoint::docs::post_document::<Arc<_>>))
+        .with_state(repo);
 
     tracing::info!("start listening on http://{}", listener.local_addr()?);
 
@@ -29,4 +39,30 @@ fn init_tracing_subscriber() {
         .with_env_filter(env_filter)
         .with_timer(tracing_subscriber::fmt::time::LocalTime::rfc_3339())
         .init();
+}
+
+use usecase::repository::*;
+
+impl<I: DocumentFileRepository> DocumentFileRepository for Arc<I> {
+    async fn store_document_file(&self, content: Vec<u8>, file_type: domain::document::DocumentFileType) -> anyhow::Result<domain::document::DocumentFile> {
+        <I as DocumentFileRepository>::store_document_file(&self, content, file_type).await
+    }
+}
+
+impl<I: DocumentRepository> DocumentRepository for Arc<I> {
+    async fn store_document(&self, document: domain::document::Document) -> anyhow::Result<()> {
+        <I as DocumentRepository>::store_document(&self, document).await
+    }
+}
+
+impl<I: FacultyRepository> FacultyRepository for Arc<I> {
+    async fn list_faculties(&self) -> anyhow::Result<Vec<domain::major::Faculty>> {
+        <I as FacultyRepository>::list_faculties(&self).await
+    }
+}
+
+impl<I: SubjectRepository> SubjectRepository for Arc<I> {
+    async fn list_subjects(&self) -> anyhow::Result<Vec<domain::subject::Subject>> {
+        <I as SubjectRepository>::list_subjects(&self).await
+    }
 }
