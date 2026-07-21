27ef6090db78bdd54427756b974339fba3ae2da7
kobita <torien2846@gmail.com>
2026-07-21 17:22:10 +0900

    scssを追加

--- frontend/src/scripts/download.ts
+++ frontend/src/scripts/download.ts
@@ -10,12 +10,13 @@ const majorSelect = document.querySelector("major-select");
 const subjectSelect = document.querySelector<SubjectSelect>("subject-select");
 const resultList = document.querySelector<HTMLUListElement>("#result-list");
 
+/**これ以降のコードの型を絞り込むため*/
 if (!form || !majorSelect || !subjectSelect || !resultList) {
     throw new Error("必要なHTML要素が見つかりません");
 }
 
 /**
- * 学部・専攻が変更されたらsubject-selectへ通知
+ * major-select の facultyIdとmajorId を subject-select の facultyIdとmajorId に反映する
  */
 majorSelect.addEventListener("selection-change", (event) => {
     const detail = (event as CustomEvent<SelectionChangeDetail>).detail;
@@ -27,16 +28,21 @@ majorSelect.addEventListener("selection-change", (event) => {
  * 検索ボタン
  */
 form.addEventListener("submit", async (event) => {
+    // 一時的に動作を止めて通信の安全性を高める
     event.preventDefault();
 
     const formData = new FormData(form);
 
     const faculty = formData.get("faculty") as string;
     const major = formData.get("major") as string;
-    const grade = Number(formData.get("grade"));
-    const term = Number(formData.get("term"));
-    const subject = formData.get("subject") as string;
+    const gradeValue = formData.get("grade") as string;
+    const termValue = formData.get("term") as string;
+    const subjectValue = formData.get("subject") as string;
+    const grade = gradeValue === "" ? undefined : Number(gradeValue);
+    const term = termValue === "" ? undefined : Number(termValue);
+    const subject = subjectValue === "" ? undefined : subjectValue;
 
+    // 検索のたびに前回検索して表示されたものを削除
     resultList.replaceChildren();
 
     try {
@@ -53,13 +59,17 @@ form.addEventListener("submit", async (event) => {
             const li = document.createElement("li");
 
             li.textContent = doc.filename;
+
+            // マウスカーソルをホバーする時用
             li.style.cursor = "pointer";
 
             li.addEventListener("click", () => {
+                // idでAPIから情報を所得する予定らしいのでidにしている
                 downloadDocument(doc.id);
             });
 
             resultList.append(li);
+            console.log("検索成功");
         }
     } catch (error) {
         console.error(error);
