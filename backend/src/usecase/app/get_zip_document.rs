717e41a00332ecaece3e4913b425422d5b17ee56
kakkak444 <gpg@mail.glabyl.com>
2026-07-23 00:26:08 +0900

    add: ドキュメントをダウンロードするUseCaseを追加

---
+++ backend/src/usecase/app/get_zip_document.rs
@@ -0,0 +1,101 @@
+use tokio::io::AsyncWriteExt;
+
+use crate::domain::{
+    Id,
+    document::{
+        Document,
+        DocumentFile,
+    },
+};
+use crate::usecase::repository::{
+    DocumentRepository,
+    DocumentFileRepository,
+};
+
+#[derive(Debug, Clone, Hash)]
+pub struct GetZipDocumentInput {
+    pub document_id: Id<Document>,
+}
+
+#[derive(Debug)]
+pub struct GetZipDocumentUseCase<I> {
+    repository: I
+}
+
+impl<I> GetZipDocumentUseCase<I> {
+    pub fn new(repository: I) -> Self {
+        Self { repository }
+    }
+}
+
+impl<I: DocumentRepository + DocumentFileRepository> GetZipDocumentUseCase<I> {
+    #[tracing::instrument(skip_all, err)]
+    pub async fn execute(&self, input: GetZipDocumentInput) -> anyhow::Result<Option<Vec<u8>>> {
+        let document_id = input.document_id;
+
+        let Some(document) = self.repository.find_document_by_id(&document_id).await?
+        else {
+            return Ok(None)
+        };
+
+        let document_zip = self.make_zip_document(document).await?;
+
+        Ok(Some(document_zip))
+    }
+
+    // 以下helper functions
+
+    #[tracing::instrument(skip_all)]
+    async fn make_zip_document(&self, document: Document) -> anyhow::Result<Vec<u8>> {
+        let mut ret = Vec::new();
+
+        let mut buf = tokio::io::BufWriter::new(&mut ret);
+        let mut zipper = async_deflate_zip::ZipWriter::new(&mut buf)
+            .with_level(async_deflate_zip::Compression::best());
+
+        for document_file in document.files() {
+            let file_name = get_file_name(document_file)?;
+
+            let entry_option = async_deflate_zip::EntryOptions {
+                mtime: std::time::SystemTime::now(),
+                permissions: None,
+                uid_gid: None,
+                comment: None,
+            };
+            let mut entry = zipper.append_file(&file_name, entry_option).await?;
+
+            let file_content = self.repository.get_document_file_content(document_file).await?;
+
+            entry.write_all(&file_content).await?;
+            entry.close().await?;
+        }
+
+        zipper.finalize().await?;
+        buf.flush().await?;
+
+        Ok(ret)
+    }
+}
+
+// 以下helper functions
+
+#[tracing::instrument(skip_all)]
+fn get_file_name(document_file: &DocumentFile) ->anyhow::Result<String> {
+    let with_extension = document_file
+        .path()
+        .with_added_extension(document_file.ty().to_string());
+
+    let Some(file_name) = with_extension.file_name()
+    else {
+        tracing::error!("document file name terminates in '..'");
+        return Err(anyhow::anyhow!("document file name terminates in '..'"));
+    };
+
+    let Some(file_name) = file_name.to_str()
+    else {
+        tracing::error!("document file name contains invalid UTF-8 chars");
+        return Err(anyhow::anyhow!("document file name contains invalid UTF-8 chars"));
+    };
+
+    Ok(file_name.to_string())
+}
--- backend/src/usecase/app/mod.rs
+++ backend/src/usecase/app/mod.rs
@@ -1,3 +1,4 @@
 pub mod get_faculties;
 pub mod get_subjects;
+pub mod get_zip_document;
 pub mod store_document;
