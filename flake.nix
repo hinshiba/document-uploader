9476fb679f86aa4877e777c57664e416150d3e49
kakkak444 <gpg@mail.glabyl.com>
2026-07-26 21:23:51 +0900

    refactor: frontendとbackendに分離

--- flake.nix
+++ flake.nix
@@ -20,22 +20,31 @@
 		};
 	in
 	{
-		devShells.${system}.default = pkgs.mkShell {
-			packages = [
-				(pkgs.rust-bin.stable.latest.default.override {
-					extensions = [ "rust-src" "rust-analyzer" ];
-				})
-				pkgs.sqlx-cli
-				pkgs.bun
-				pkgs.tera-cli
-				pkgs.superhtml
-			];
-			shellHook = ''
-				# Run safe-chain bash initialisation script
-				if [ -d "./.local/safe-chain" ]; then
-					source ./.local/safe-chain/scripts/init-posix.sh
-				fi
-			'';
+		devShells.${system} = rec {
+			default = pkgs.mkShell {
+				inputsFrom = [ frontend backend ];
+			};
+			backend = pkgs.mkShell {
+				packages = [
+					(pkgs.rust-bin.stable.latest.default.override {
+						extensions = [ "rust-src" "rust-analyzer" ];
+					})
+					pkgs.sqlx-cli
+				];
+			};
+			frontend = pkgs.mkShell {
+				packages = [
+					pkgs.bun
+					pkgs.tera-cli
+					pkgs.superhtml
+				];
+				shellHook = ''
+					# Run safe-chain bash initialisation script
+					if [ -d "./.local/safe-chain" ]; then
+						source ./.local/safe-chain/scripts/init-posix.sh
+					fi
+				'';
+			};
 		};
 	};
 }
