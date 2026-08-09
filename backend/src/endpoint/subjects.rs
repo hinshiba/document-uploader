7f2e52cffc90c2756c5957d72bb9945e6719e90d
kakkak444 <gpg@mail.glabyl.com>
2026-08-09 23:23:39 +0900

    add: POST /subjectsに対するハンドラを実装

--- backend/src/endpoint/subjects.rs
+++ backend/src/endpoint/subjects.rs
@@ -1,9 +1,13 @@
 use axum::{
     extract::{
+        self,
         Query,
         State,
     },
-    http::StatusCode,
+    http::{
+        header,
+        StatusCode,
+    },
     response::{
         IntoResponse,
         Json,
@@ -14,12 +18,27 @@ use serde::{
     Serialize,
 };
 
-use crate::usecase::{
-    app::get_subjects::{
-        GetSubjectsOption,
-        GetSubjectsUseCase,
+use crate::{
+    domain::{
+        Id,
+        Grade,
+        Term,
+        subject::Subject,
     },
-    repository::SubjectRepository,
+    usecase::{
+        app::{
+            create_subject::{
+                CreateSubjectInput,
+                CreateSubjectOutput,
+                CreateSubjectUseCase,
+            },
+            get_subjects::{
+                GetSubjectsOption,
+                GetSubjectsUseCase,
+            },
+        },
+        repository::SubjectRepository,
+    },
 };
 use super::{
     dto::subject::SubjectDto,
@@ -43,6 +62,31 @@ impl Input {
             grade: self.grade,
             term: self.term,
         }
+    }
+}
+
+#[derive(Debug, Clone, Hash, Deserialize, Serialize)]
+pub struct PostInput {
+    name: String,
+    course_code: String,
+    faculty: uuid::Uuid,
+    major: uuid::Uuid,
+    grade: i64,
+    term: i64,
+}
+
+impl PostInput {
+    fn to_create_subject_input(&self) -> Result<CreateSubjectInput, EndpointError> {
+        let create_subject_input = CreateSubjectInput {
+            name: self.name.clone(),
+            course_code: self.course_code.clone(),
+            faculty_id: Id::new(self.faculty),
+            major_id: Id::new(self.major),
+            grade: Grade::new(self.grade).map_err(parse_error_to_endpoint_error)?,
+            term: Term::new(self.term).map_err(parse_error_to_endpoint_error)?,
+        };
+
+        Ok(create_subject_input)
     }
 }
 
@@ -76,4 +120,96 @@ pub async fn get_subjects<I: SubjectRepository>(
                 .collect::<Vec<_>>()
         ))
     )
+}
+
+#[tracing::instrument(skip(repo), ret(level="info"))]
+pub async fn post_subject<I: SubjectRepository>(
+    State(repo): State<I>,
+    extract::Json(input): extract::Json<PostInput>,
+) -> EndpointResult<impl IntoResponse> {
+    let create_subject_input = match input.to_create_subject_input() {
+        Ok(ret) => ret,
+        Err(err) => return error_with_400(err),
+    };
+
+    match CreateSubjectUseCase::new(repo)
+            .execute(create_subject_input)
+            .await
+            .map(convert_create_subject_output)
+    {
+        Ok(Ok(subject)) => {
+            ( StatusCode::CREATED
+            , Ok(([(header::LOCATION, format!("/subjects/{}", subject.id().id().as_hyphenated()))
+                  ],
+                  Json(SubjectDto {
+                    id: subject.id().id().clone(),
+                    name: subject.name().to_owned(),
+                    faculty: subject.faculty_id().id().clone(),
+                    major: subject.major_id().id().clone(),
+                    grade: subject.grade().grade().clone(),
+                    term: subject.term().term().clone()
+                  })
+            )))
+        },
+        Ok(Err(err)) => error_with_400(err),
+        Err(err) => {
+            tracing::error!("{}", err);
+
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Err(EndpointError {
+                    message: "unexpected error occured".to_owned(),
+                    details: None,
+                })
+            )
+        },
+    }
+}
+
+// 以下 helper functions
+
+#[inline]
+fn parse_error_to_endpoint_error(e: impl std::error::Error) -> EndpointError {
+    EndpointError {
+        message: "validation error".to_owned(),
+        details: Some(e.to_string())
+    }
+}
+
+#[inline]
+fn error_with_400<T>(e: EndpointError) -> EndpointResult<T> {
+    (StatusCode::BAD_REQUEST, Err(e))
+}
+
+#[inline]
+fn convert_create_subject_output(o: CreateSubjectOutput) -> Result<Subject, EndpointError> {
+    use CreateSubjectOutput::*;
+
+    match o {
+        Created(subject) => Ok(subject),
+        ErrCourseCodeNonUnique(course_code) => Err(
+            EndpointError {
+                message: "duplicate check failed".to_owned(),
+                details: Some(format!("subject which have course_code '{}' already exists", course_code)),
+            }
+        ),
+        ErrFacultyNotExist(_faculty_id) => Err(
+            EndpointError {
+                message: "invalid faculty_id".to_owned(),
+                details: Some("faculty does not exist".to_owned()),
+            }
+        ),
+        ErrMajorNotExist(_major_id) => Err(
+            EndpointError {
+                message: "invalid major_id".to_owned(),
+                details: Some("major does not exist".to_owned()),
+            }
+        ),
+        ErrFacultyMajorRelation(_faculty_id, _major_id) => Err(
+            EndpointError {
+                message: "invalid major_id".to_owned(),
+                details: Some("major does not belong to faculty".to_owned()),
+            }
+        ),
+    }
 }
