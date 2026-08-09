1d26116d7744721278a93b3ceca57ee36fa4cdd8
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-08-10 03:19:28 +0900

    feat: 非同期でu8をfsから読み書きする実装の追加

--- backend/src/infrastructure/local_filesystem.rs
+++ backend/src/infrastructure/local_filesystem.rs
@@ -1,1 +1,58 @@
+use std::path::PathBuf;
 
+use tokio::io::{AsyncWriteExt, BufWriter};
+
+use crate::{
+    domain::document::{DocumentFile, DocumentFileType},
+    usecase::repository::DocumentFileRepository,
+};
+
+/// ローカルファイルシステムにドキュメントの実体を保存するためのリポジトリ実装
+#[derive(Debug)]
+pub struct LocalFileSystem {
+    /// 保存のルートとなるディレクトリ
+    save_dir: PathBuf,
+}
+
+impl LocalFileSystem {
+    /// `save_dir`をルートとするドキュメントの実体を保存するリポジトリを作成する
+    ///
+    /// # Errors
+    ///
+    /// `save_dir`が`fs::create_dir_all`で作成できない場合はエラーとなる
+    pub fn new(save_dir: PathBuf) -> std::io::Result<Self> {
+        if !save_dir.exists() {
+            // すでに存在する場合はエラー扱いなので事前検証
+            std::fs::create_dir_all(&save_dir)?;
+        }
+
+        Ok(Self { save_dir })
+    }
+}
+
+impl DocumentFileRepository for LocalFileSystem {
+    #[tracing::instrument(skip_all, ret(level = "info"), err)]
+    async fn store_document_file(
+        &self,
+        content: Vec<u8>,
+        file_type: DocumentFileType,
+    ) -> anyhow::Result<DocumentFile> {
+        let file_name = uuid::Uuid::new_v4().to_string();
+        let file_path = self.save_dir.join(file_name);
+
+        let mut buffer = BufWriter::new(tokio::fs::File::create_new(&file_path).await?);
+        buffer.write_all(&content).await?;
+        buffer.flush().await?;
+
+        Ok(DocumentFile::new(file_type, file_path))
+    }
+
+    #[tracing::instrument(skip(self), err)]
+    async fn get_document_file_content(
+        &self,
+        document_file: &DocumentFile,
+    ) -> anyhow::Result<Vec<u8>> {
+        let content = tokio::fs::read(document_file.path()).await?;
+        Ok(content)
+    }
+}
