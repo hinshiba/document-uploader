9f7b679d50b75d8b0f73af47e94b5019b8f354d7
kakkak444 <172775100+kakkak444@users.noreply.github.com>
2026-08-11 21:52:31 +0900

    add: `DocumentRepository`に`search_documents`を追加

--- backend/src/usecase/repository.rs
+++ backend/src/usecase/repository.rs
@@ -2,15 +2,48 @@ use crate::domain::{
     Id,
     Grade,
     Term,
+    Year,
     document::{
         Document,
+        DocumentMetadata,
         DocumentFile,
         DocumentFileType,
+        ExamType,
     },
     faculty::Faculty,
     major::Major,
     subject::Subject,
 };
+
+#[derive(Debug, Hash)]
+pub struct SearchDocumentOption {
+    pub subject_id: Id<Subject>,
+    pub year: Option<Year<DocumentMetadata>>,
+    pub teacher: Option<String>,
+    pub exam_type: Option<ExamType>,
+    pub is_answer: Option<bool>,
+}
+
+impl SearchDocumentOption {
+    /// # Example:
+    /// ```
+    /// let a = SearchDocumentOption {
+    ///     year: Some(Year::new(2026).unwrap()),
+    ///     ..SearchDocumentOption::minimal(Id::new(uuid::Uuid::new_v4()))
+    /// }
+    ///
+    /// assert_eq!(a.year.as_ref().map(Year::year), Some(&2026));
+    /// ```
+    pub fn minimal(subject_id: Id<Subject>) -> Self {
+        Self {
+            subject_id,
+            year: None,
+            teacher: None,
+            exam_type: None,
+            is_answer: None,
+        }
+    }
+}
 
 #[derive(Debug, Hash)]
 pub struct UpdateSubjectContent {
@@ -33,6 +66,7 @@ pub struct SearchSubjectOption {
 
 pub trait DocumentRepository: Send + Sync {
     fn find_document_by_id(&self, document_id: &Id<Document>) -> impl Future<Output=anyhow::Result<Option<Document>>> + Send;
+    fn search_documents(&self, option: SearchDocumentOption) -> impl Future<Output=anyhow::Result<Vec<Document>>> + Send;
     fn store_document(&self, document: Document) -> impl Future<Output=anyhow::Result<()>> + Send;
 }
 
@@ -53,22 +87,43 @@ pub trait SubjectRepository: Send + Sync {
 
     /// # Returns
     /// 作成処理が完了したとき、[`Ok`]を返す
-    /// 
+    ///
     /// # Errors
     /// `subject.id`と等しいidを持つ[`Subject`]が既に存在する場合はエラーを返す
     fn create_subject(&self, subject: Subject) -> impl Future<Output=anyhow::Result<()>> + Send;
 
     /// # Returns
     /// 更新処理が完了したとき、更新後の[`Subject`]を[`Ok`]に包んで返す
-    /// 
+    ///
     /// # Errors
     /// `subject_id`に対応する[`Subject`]が存在しないときはエラーを返す
     fn update_subject(&self, subject_id: Id<Subject>, content: UpdateSubjectContent) -> impl Future<Output=anyhow::Result<Subject>> + Send;
-    
+
     /// # Returns
     /// 削除処理が完了したとき、削除前の`subject_id`に対応する[`Subject`]を[`Ok`]に包んで返す
-    /// 
+    ///
     /// # Errors
     /// `subject_id`に対応する[`Subject`]が存在しないときはエラーを返す
     fn delete_subject(&self, subject_id: Id<Subject>) -> impl Future<Output=anyhow::Result<Subject>> + Send;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_document_option() {
+        let a = SearchDocumentOption::minimal(Id::new(uuid::Uuid::new_v4()));
+
+        dbg!(a);
+
+        let b = SearchDocumentOption {
+            year: Some(Year::new(2026).unwrap()),
+            ..SearchDocumentOption::minimal(Id::new(uuid::Uuid::new_v4()))
+        };
+
+        dbg!(&b);
+
+        assert_eq!(b.year.as_ref().map(Year::year), Some(&2026));
+    }
 }
