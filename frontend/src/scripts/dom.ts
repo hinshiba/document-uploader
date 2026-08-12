f396d78ac9ab2ef6c9b6f7379874e248148b4de0
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-08-12 18:09:40 +0900

    clean: 不要なputerrorを削除

--- frontend/src/scripts/dom.ts
+++ frontend/src/scripts/dom.ts
@@ -2,8 +2,6 @@
  * 共通して行うdom操作を集約する
  */
 
-import { toUserMessage } from "./error";
-
 /**
  * 要素を型付きで取得するヘルパ
  * @param selector セレクタ
@@ -16,11 +14,4 @@ export function required<T extends Element>(selector: string): T {
         throw new Error(`Element not found. selector: ${selector}`);
     }
     return el;
-}
-
-/**
- * 指定要素の`textContent`にエラーメッセージを出力する
- */
-export function putError(element: HTMLElement, err: unknown): void {
-    element.textContent = toUserMessage(err);
 }
