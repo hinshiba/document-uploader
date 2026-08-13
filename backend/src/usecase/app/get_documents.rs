fc965f11176b712307a6accf78dfe016c16f8297
kakkak444 <172775100+kakkak444@users.noreply.github.com>
2026-08-13 16:32:24 +0900

    add: 条件に合うドキュメントを取得するユースケースを追加

---
+++ backend/src/usecase/app/get_documents.rs
@@ -0,0 +1,58 @@
+use crate::{
+    domain::{
+        Id,
+        Year,
+        document::{
+            Document,
+            DocumentMetadata,
+            ExamType,
+        },
+    },
+    usecase::repository::{
+        SearchDocumentOption,
+        DocumentRepository,
+    },
+};
+
+#[derive(Debug)]
+pub struct GetDocumentsUseCase<I> {
+    repository: I
+}
+
+#[derive(Debug, Clone, Hash)]
+pub struct GetDocumentsOption {
+    pub subject_id: uuid::Uuid,
+    pub year: Option<i64>,
+    pub teacher: Option<String>,
+    pub exam_type: Option<ExamType>,
+    pub is_answer: Option<bool>,
+}
+
+impl<I> GetDocumentsUseCase<I> {
+    pub fn new(repository: I) -> Self {
+        Self { repository }
+    }
+}
+
+impl<I: DocumentRepository> GetDocumentsUseCase<I> {
+    #[tracing::instrument(skip(self), ret(level="debug"), err)]
+    pub async fn execute(&self, option: GetDocumentsOption) -> anyhow::Result<Vec<Document>> {
+        // `option.year`の検証に失敗したら空配列を返す
+        let Ok(option_year) = option.year.map(|y| Year::new(y)).transpose()
+        else {
+            return Ok(vec![]);
+        };
+
+        let repo_option = SearchDocumentOption {
+            subject_id: Id::new(option.subject_id),
+            year: option_year,
+            teacher: option.teacher,
+            exam_type: option.exam_type,
+            is_answer: option.is_answer,
+        };
+
+        let result_vec = self.repository.search_documents(repo_option).await?;
+
+        Ok(result_vec)
+    }
+}
--- backend/src/usecase/app/mod.rs
+++ backend/src/usecase/app/mod.rs
@@ -3,5 +3,6 @@ pub mod create_subject;
 pub mod delete_subject;
 pub mod update_subject;
 pub mod get_subjects;
+pub mod get_documents;
 pub mod get_zip_document;
 pub mod store_document;
