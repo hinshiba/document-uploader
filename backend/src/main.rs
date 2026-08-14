527e570245a81a99e5aa4972837c4ecc41d0780b
kakkak444 <172775100+kakkak444@users.noreply.github.com>
2026-08-15 01:09:11 +0900

    add: `ExampleRepository`を使用するように変更

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,12 @@
 //! document-uploaderのエントリポイント
-//! 
+//!
 //! # 必要な環境変数
-//! 
+//!
 //! - LISTEN_ADDR
 //! - DATABASE_URL
-//! 
+//!
 //! # Notes
-//! 
+//!
 //! `./public/` にあるファイルが配信される
 //!
 //! `./save_dir`にすべてのアップロードされたファイルは格納される
@@ -17,7 +17,7 @@ use anyhow::Context;
 use tower_http::services::ServeDir;
 
 use crate::{
-    endpoint::api::api_router, 
+    endpoint::api::api_router,
     infrastructure::{
         local_fs::LocalFileSystemRepository,
         pgfs::PgFsRepository,
@@ -30,6 +30,9 @@ mod endpoint;
 mod infrastructure;
 mod usecase;
 
+use std::sync::Arc;
+use infrastructure::repository::ExampleRepository;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // 同名の環境変数が登録されている場合はそちらが優先されることに注意
@@ -40,10 +43,7 @@ async fn main() -> anyhow::Result<()> {
     let pgpool = sqlx::PgPool::connect(&env::var("DATABASE_URL").context("DATABASE_URL is not set.")?).await?;
 
 
-    let state = PgFsRepository::new(
-        PostgresRepository::new(pgpool), 
-        LocalFileSystemRepository::new("save_dir".into())?
-    );
+    let state = Arc::new(init_example_repository()?);
 
     let app = axum::Router::new()
         .nest("/api/v1", api_router(state))
@@ -66,4 +66,54 @@ fn init_tracing_subscriber() {
         .with_env_filter(env_filter)
         .with_timer(tracing_subscriber::fmt::time::LocalTime::rfc_3339())
         .init();
+}
+
+fn init_example_repository() -> anyhow::Result<ExampleRepository> {
+    let save_dir = std::path::PathBuf::from("save_dir");
+
+    Ok(ExampleRepository::new(save_dir)?)
+}
+
+use usecase::repository::*;
+
+impl<I: DocumentFileRepository> DocumentFileRepository for Arc<I> {
+    async fn store_document_file(&self, content: Vec<u8>, file_type: domain::document::DocumentFileType) -> anyhow::Result<domain::document::DocumentFile> {
+        <I as DocumentFileRepository>::store_document_file(&self, content, file_type).await
+    }
+    async fn get_document_file_content(&self, document_file: &domain::document::DocumentFile) -> anyhow::Result<Vec<u8>> {
+        <I as DocumentFileRepository>::get_document_file_content(&self, document_file).await
+    }
+}
+
+impl<I: DocumentRepository> DocumentRepository for Arc<I> {
+    async fn store_document(&self, document: domain::document::Document) -> anyhow::Result<()> {
+        <I as DocumentRepository>::store_document(&self, document).await
+    }
+    async fn find_document_by_id(&self, document_id: &domain::Id<domain::document::Document>) -> anyhow::Result<Option<domain::document::Document>> {
+        <I as DocumentRepository>::find_document_by_id(&self, document_id).await
+    }
+}
+
+impl<I: FacultyRepository> FacultyRepository for Arc<I> {
+    async fn list_faculties(&self) -> anyhow::Result<Vec<domain::major::Faculty>> {
+        <I as FacultyRepository>::list_faculties(&self).await
+    }
+}
+
+impl<I: SubjectRepository> SubjectRepository for Arc<I> {
+    async fn list_subjects(&self) -> anyhow::Result<Vec<domain::subject::Subject>> {
+        <I as SubjectRepository>::list_subjects(&self).await
+    }
+    async fn create_subject(&self, subject: domain::subject::Subject) -> anyhow::Result<()> {
+        <I as SubjectRepository>::create_subject(&self, subject).await
+    }
+    async fn delete_subject(&self, subject_id: domain::Id<domain::subject::Subject>) -> anyhow::Result<domain::subject::Subject> {
+        <I as SubjectRepository>::delete_subject(&self, subject_id).await
+    }
+    async fn search_subjects(&self, option: SearchSubjectOption) -> anyhow::Result<Vec<domain::subject::Subject>> {
+        <I as SubjectRepository>::search_subjects(&self, option).await
+    }
+    async fn update_subject(&self, subject_id: domain::Id<domain::subject::Subject>, content: UpdateSubjectContent) -> anyhow::Result<domain::subject::Subject> {
+        <I as SubjectRepository>::update_subject(&self, subject_id, content).await
+    }
 }
